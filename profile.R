0ac3d732f5342f7d9851600e4b751d2f7a2d9341
jackcamilleri <hfm191@vols.utk.edu>
2024-04-02 12:15:48 -0400

    added slow code

--- profile.R
+++ profile.R
@@ -1,7 +1,21 @@
-x = matrix(runif(1e5*500), ncol = 500)
-
-
 Rprof()
-x_pr = prcomp(x, retx = FALSE)
+
+system.time({
+library(dplyr)
+library(lubridate)
+library(readxl)
+po_line <- read_excel("po_line.xlsx")
+on_hand <- read_excel("on_hand.xlsx")
+
+on_hand$week <- as.numeric(substr(on_hand$dt, 5, 6))
+po_line$crtd_dt <- as.Date(po_line$crtd_dt, "%Y-%m-%d")
+po_line$recpt_dt <- as.Date(po_line$recpt_dt, "%Y-%m-%d")
+po_line$week_due <- as.numeric(strftime(po_line$due_dt, format = '%V'))
+po_line$week_promised_receipt <- as.numeric(strftime(po_line$ab_dt, format = '%V'))
+po_line$week_received <- as.numeric(strftime(po_line$recpt_dt, format = '%V'))
+po_line$week_created <- as.numeric(strftime(po_line$crtd_dt, format = '%V'))
+po_line$lead_time <- po_line$recpt_dt - po_line$crtd_dt
+
+})
 Rprof(NULL)
 summaryRprof()
