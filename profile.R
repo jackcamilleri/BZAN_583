864fd062c2ede6000555ed90223f7aef39847b05
jackcamilleri <hfm191@vols.utk.edu>
2024-04-02 13:09:59 -0400

    modified fast code

--- profile.R
+++ profile.R
@@ -4,16 +4,16 @@ system.time({
 library(dplyr)
 library(lubridate)
 library(readxl)
-po_line <- read_excel("po_line.xlsx")
-on_hand <- read_excel("on_hand.xlsx")
-
+po_line <- read_xlsx("po_line.xlsx")
+on_hand <- read_xlsx("on_hand.xlsx")
+  
 on_hand$week <- as.numeric(substr(on_hand$dt, 5, 6))
 po_line$crtd_dt <- as.Date(po_line$crtd_dt, "%Y-%m-%d")
 po_line$recpt_dt <- as.Date(po_line$recpt_dt, "%Y-%m-%d")
-po_line$week_due <- as.numeric(strftime(po_line$due_dt, format = '%V'))
-po_line$week_promised_receipt <- as.numeric(strftime(po_line$ab_dt, format = '%V'))
-po_line$week_received <- as.numeric(strftime(po_line$recpt_dt, format = '%V'))
-po_line$week_created <- as.numeric(strftime(po_line$crtd_dt, format = '%V'))
+po_line$week_due <- week(po_line$due_dt)
+po_line$week_promised_receipt <- week(po_line$ab_dt)
+po_line$week_received <- week(po_line$recpt_dt)
+po_line$week_created <- week(po_line$crtd_dt)
 po_line$lead_time <- po_line$recpt_dt - po_line$crtd_dt
 
 })
